a75d2060a70aa1135245016346900442f7b537e7
Jainsi1 <75137939+Jainsi1@users.noreply.github.com>
2023-03-20 19:03:38 +0530

    Update index.py

--- python/index.py
+++ python/index.py
@@ -5,4 +5,4 @@ def run():
     return "{\"HELLO WORLD\"}"
 
 if __name__=="__main__":
-    helloworld.run(host="34.207.189.0", port=int("4000"), debug=True)
+    helloworld.run(host="0.0.0/0", port=int("4000"), debug=True)
